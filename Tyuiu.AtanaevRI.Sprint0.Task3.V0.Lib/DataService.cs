7f2a2a34255440157d6c7db8c9c885fda037bbd6
atanaeva_gg@mail.ru <atanaeva_gg@mail.ru>
2025-09-20 00:08:09 +0500

    Task3

---
+++ Tyuiu.AtanaevRI.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.AtanaevRI.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
