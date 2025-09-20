ebad932e57835f73b908a09e58c5415a54d61295
atanaeva_gg@mail.ru <atanaeva_gg@mail.ru>
2025-09-20 10:11:10 +0500

    Task5

---
+++ Tyuiu.AtanaevRI.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.AtanaevRI.Sprint0.Task5.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("А + B =" + DataService.Addition(1, 5));
+        Console.WriteLine("А - B =" + DataService.Subtraction(15, 5));
+        Console.WriteLine("А * B =" + DataService.Multiplication(10, 10));
+        Console.WriteLine("А / B =" + DataService.Division(5, 0));
+        Console.ReadKey();
+    }
+}
