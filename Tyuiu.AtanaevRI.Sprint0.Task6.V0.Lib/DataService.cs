4349bfe033ddd4dab2b501b649797c0c12bfffe2
atanaeva_gg@mail.ru <atanaeva_gg@mail.ru>
2025-09-20 11:39:31 +0500

    Task6

---
+++ Tyuiu.AtanaevRI.Sprint0.Task6.V0.Lib/DataService.cs
@@ -0,0 +1,42 @@
+﻿namespace Tyuiu.AtanaevRI.Sprint0.Task6.V0.Lib
+{
+    public class DataService
+    {
+        // Сумма элементов массива
+        public static int AdditionArray(int[] numbers)
+        {
+            var total = 0;
+            for (var index = 0; index < numbers.Length; index++)
+            {
+                total += numbers[index];
+            }
+            return total;
+        }
+
+        // Разность элементов массива
+        public static int SubstractionArray(int[] numbers)
+        {
+            var total = 0;
+            int index = 0;
+            while (index < numbers.Length)
+            {
+                total -= numbers[index];
+                index++;
+            }
+            return total;
+        }
+
+        // Произведение элементов массива
+        public static int MultiplicationArray(int[] numbers)
+        {
+            int total = 1;
+            int index = 0;
+            do
+            {
+                total *= numbers[index];
+                index++;
+            } while (index < numbers.Length);
+            return total;
+        }
+    }
+}
---
+++ Tyuiu.AtanaevRI.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Tyuiu.AtanaevRI.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.AtanaevRI.Sprint0.Task6.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayNums = new int[] { 1, 2, 3, 4, 5 };
+
+            // Пример циклической структуры: сложение элементов массива
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(arrayNums));
+
+            // Пример циклической структуры: вычитание элементов массива
+            Console.WriteLine("Разность элементов массива = " + DataService.SubstractionArray(arrayNums));
+
+            // Пример циклической структуры: произведение элементов массива
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(arrayNums));
+
+            Console.ReadKey();
+        }
+    }
+}
