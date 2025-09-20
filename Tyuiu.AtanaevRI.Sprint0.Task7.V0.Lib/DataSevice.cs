8837f5f4a24884f2ea06928f039b7697f0223840
atanaeva_gg@mail.ru <atanaeva_gg@mail.ru>
2025-09-20 11:48:08 +0500

    Task0

---
+++ Tyuiu.AtanaevRI.Sprint0.Task7.V0.Lib/DataSevice.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.AtanaevRI.Sprint0.Task7.V0.Lib
+{
+    public class DataSevice
+    {
+
+    }
+}
---
+++ Tyuiu.AtanaevRI.Sprint0.Task7.V0.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.AtanaevRI.Sprint0.Task7.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
