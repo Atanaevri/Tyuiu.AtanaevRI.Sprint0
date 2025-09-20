345aef070df94d08235f2ddb08116f4c0c411011
atanaeva_gg@mail.ru <atanaeva_gg@mail.ru>
2025-09-20 11:54:07 +0500

    Task1

---
+++ Tyuiu.AtanaevRI.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tyuiu.AtanaevRI.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string name = "Роман";
+            int age = 18;
+
+            Console.WriteLine("Здравствуйте, меня зовут " + name + ", мне " + age + " лет.");
+
+            Console.ReadKey();
+        }
+    }
+}
